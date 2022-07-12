0bce882ce5d8eee4edebb0a06554c358f6ea9863
Thomas Chan <google-oauth2|111834764151316401451>
2022-07-12 09:10:51 +0000

    changes

---
+++ includes/test_var.js
@@ -0,0 +1,8 @@
+// Read more in the JS API documentation here: https://docs.dataform.co/guides/javascript/js-api
+
+// publish("new_table")
+//   .type("table")
+//   .query(ctx => `select * from ${ctx.ref("source_table")}`);
+
+var a = ["dataform.testing1", "dataform.testing2"];
+module.exports = {a};
